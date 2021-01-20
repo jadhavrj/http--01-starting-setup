d9bd6f6357e78ecf6101da6e8513f71495f150b3
RUTURAJPC\rutur <jadhavrj@mail.uc.edu>
2021-01-19 20:46:29 -0500

    Active Link Style and Extracting RouteParams (203)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from '../../containers/Blog/Posts/Posts';
+import NewPost from '../../containers/Blog/NewPost/NewPost';
+import FullPost from  '../Blog/FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -13,14 +15,27 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><NavLink 
+                            to="/" 
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                            }   }>Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: 'quick-submit=true'
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
             </div>
         );
     }
