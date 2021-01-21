ff1e250df6431778672474d478ede4ae2ec863e1
RUTURAJPC\rutur <jadhavrj@mail.uc.edu>
2021-01-20 21:43:52 -0500

    Added Nested Routing (209)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,7 +5,6 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from '../../containers/Blog/Posts/Posts';
 import NewPost from '../../containers/Blog/NewPost/NewPost';
-import FullPost from  '../Blog/FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -16,7 +15,7 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink 
-                            to="/" 
+                            to="/posts" 
                                 exact
                                 activeClassName="my-active"
                                 activeStyle={{
@@ -33,10 +32,9 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-                <Route path="/" exact component={Posts} />
                 <Switch>
                     <Route path="/new-post" component={NewPost} />
-                    <Route path="/:id" exact component={FullPost} />
+                    <Route path="/posts" component={Posts} />
                 </Switch>
             </div>
         );
