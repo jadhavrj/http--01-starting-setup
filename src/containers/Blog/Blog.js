a99cb936f225ea82b16ce5736394c91529f69a0e
RUTURAJPC\rutur <jadhavrj@mail.uc.edu>
2021-01-21 08:18:55 -0500

    Lazy Route Loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
-import Posts from '../../containers/Blog/Posts/Posts';
-import NewPost from '../../containers/Blog/NewPost/NewPost';
+import Posts from './Posts/Posts';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -36,7 +41,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>Not Found</h1>} />
                     {/* <Redirect from="/" to="/posts" /> */}
